f3b4960e12a03e2e9100e1b186d692610f4c782a
Dhwaj Sharma <dhwajsharma10@gmail.com>
2021-03-02 12:18:55 +0530

    implemented api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import "./styles.css";
+import React, { useState, useEffect } from "react";
+
+const API_URL = "http://api.icndb.com/jokes/random";
 
 export default function App() {
+  const [joke, setJoke] = useState("");
+
+  const generateJoke = () => {
+    fetch(API_URL)
+      .then(res => res.json())
+      .then(data => setJoke(data.value.joke))
+  }
+
+  useEffect(() => {
+    generateJoke();
+  }, [])
   return (
     <div className="Box">
       <h3>Chuck Norris Jokes</h3>
-      <p>here goes the joke</p>
-      <button>Get a new Joke </button>
-
+      <p dangerouslySetInnerHTML={{ __html: joke }} />
+      <button onClick={generateJoke}>Get a new Joke </button>
     </div>
   );
 }
