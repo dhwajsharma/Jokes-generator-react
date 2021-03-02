410519528e400ec1f736d776d65eb5f39a83d810
Dhwaj Sharma <dhwajsharma10@gmail.com>
2021-03-02 12:02:53 +0530

    done with css

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="Box">
+      <h3>Chuck Norris Jokes</h3>
+      <p>here goes the joke</p>
+      <button>Get a new Joke </button>
+
+    </div>
+  );
+}
